588b0ab3d16e83570d69c84b30e15fa9f8693de7
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-17 02:32:04 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,2 +1,27 @@
-mv delivery/apples fruit
-mv delivery/
+mv delivery/apples.txt fruit
+mv delivery/bison_burgers.txt
+mv delivery/brie.txt
+mv delivery/broccoli.txt
+mv carrot_cake.txt
+mv carrots.txt 
+mv eggo_waffles.txt
+mv fage_yogurt.txt
+mv fig_bars.txt
+mv flourless_cake.txt
+mv garlic.txt
+mv goldfish.txt
+mv grapples.txt
+mv hotdogs.txt
+mv icecream.txt
+mv lamb_ribs.txt
+mv land_o_lakes_butter.txt
+mv lean_cuisine.txt
+mv leeks.txt
+mv milk.txt
+mv mint_milano_cookies.txt
+mv onion.txt
+mv oreos.txt
+mv potatoes.txt
+mv salmon_steak.txt
+mv tiramisu_cake.txt
+mv tomatioes.txt
